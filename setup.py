3dd77d2c3a6c4f8f3e4820ca792720f3100d368b
schnellf <schnellf@f5520b42-8414-0410-9ae7-91ee0cd12377>
2007-06-28 14:14:38 +0000

    clean-up installer to search for images in the images folder.
    git-svn-id: https://subversion.renater.fr/audiovideocours/trunk/client_and_standalone@330 f5520b42-8414-0410-9ae7-91ee0cd12377

--- setup.py
+++ setup.py
@@ -1,3 +1,9 @@
+################################################################################
+# This is the py2exe setup file to create an .exe build 
+# for audiovideocours projects.
+#
+# Usage: python setup.py py2exe
+################################################################################
 
 from distutils.core import setup
 import py2exe
